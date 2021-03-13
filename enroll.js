cb787769b9ff2eb2bed8e139f1f2c658f5510253
葛驰 <67827764+garbagechi@users.noreply.github.com>
2021-03-13 08:18:14 +0800

    Add files via upload

---
+++ enroll.js
@@ -0,0 +1,10 @@
+
+var enroll = document.getElementById("enroll");
+alert(enroll);
+enroll.onclick = function(e)
+{
+    e.preventDefault();
+    enroll.innerHTML = "报名成功";
+    enroll.style.background="#27cb8b";
+    enroll.style.borderColor = "#27cb8b"
+}
